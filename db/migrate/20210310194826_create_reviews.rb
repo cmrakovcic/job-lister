58ec6caa8ffc052e842671a70f14f0d9e317fb15
Christopher Mrakovcic <cmrakovcic@aol.com>
2021-03-10 15:27:35 -0500

    migrated db

--- db/migrate/20210310194826_create_reviews.rb
+++ db/migrate/20210310194826_create_reviews.rb
@@ -2,8 +2,8 @@ class CreateReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :reviews do |t|
       t.string :content
-      t.belongs_to :user, null: false, foreign_key: true
-      t.belongs_to :job, null: false, foreign_key: true
+      t.references :user, foreign_key: true
+      t.references :job, foreign_key: true
 
       t.timestamps
     end
