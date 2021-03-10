e5cbf1816626d02d44dfd3ebb5f8e509beccabaf
Christopher Mrakovcic <cmrakovcic@aol.com>
2021-03-10 15:39:14 -0500

    added category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_10_194826) do
+ActiveRecord::Schema.define(version: 2021_03_10_203618) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "jobs", force: :cascade do |t|
     t.string "title"
