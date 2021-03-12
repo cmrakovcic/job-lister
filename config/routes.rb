7aa46a8b6e0c40ac66c253c429a9c7db280a2304
Christopher Mrakovcic <cmrakovcic@aol.com>
2021-03-12 10:54:40 -0500

    added pry to gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,19 @@
 Rails.application.routes.draw do
   root "sessions#home"
-
+ 
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
-
-  # login
+ 
+  #login route
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
-
-  # logout
+ 
+  #logout route
   delete '/logout' => 'sessions#destroy'
 
-  resources :reviews
-  resources :users
-  resources :jobs
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+   resources :reviews
+   resources :users
+   resources :jobs
+
+   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ end
