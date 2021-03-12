305dfd2da109668ef35d193821b9a7ed987b6a11
Christopher Mrakovcic <cmrakovcic@aol.com>
2021-03-12 11:43:51 -0500

    added nested resources in routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,12 @@ Rails.application.routes.draw do
   delete '/logout' => 'sessions#destroy'
 
    resources :reviews
-   resources :users
-   resources :jobs
+   resources :users do
+     resources :jobs, only: [:new, :create, :index]
+   end
+   resources :jobs do
+     resources :reviews, only: [:new, :create, :index]
+   end
 
    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  end
