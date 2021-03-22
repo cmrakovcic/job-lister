47b9a9ce2b61e6feb1daf9ec8d9ce7bab049707f
Christopher Mrakovcic <cmrakovcic@aol.com>
2021-03-22 12:23:02 -0400

    added category association to job model

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,10 @@
 class Job < ApplicationRecord
   belongs_to :user
+  belongs_to :category
   has_many :reviews
   has_many :users, through: :reviews
+
+  def category_attributes=(attr)
+    self.category = Category.find_or_create_by(attr) if !attr[:name].blank?
+  end
 end
