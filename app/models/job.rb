4b30d717f254d7e043893d7e66473ad6ba2f0ac8
Christopher Mrakovcic <cmrakovcic@aol.com>
2021-03-22 14:57:40 -0400

    fixed my additional scope method

--- app/models/job.rb
+++ app/models/job.rb
@@ -5,7 +5,7 @@ class Job < ApplicationRecord
   has_many :users, through: :reviews
 
   scope :alpha, -> { order(:title) }
-  scope :most_reviews, -> { joins(:reviews).group('jobs.id').order('count(jobs.id) desc') }
+  scope :most_reviews, -> { left_joins(:reviews).group('jobs.id').order('count(reviews.job.id) desc') }
 
   def category_attributes=(attr)
     self.category = Category.find_or_create_by(attr) if !attr[:name].blank?
