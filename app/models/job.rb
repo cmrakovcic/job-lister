89d262ef787f3a9ed3099c7ff550204eef57d16b
Christopher Mrakovcic <cmrakovcic@aol.com>
2021-03-22 12:51:46 -0400

    added scope method to job model

--- app/models/job.rb
+++ app/models/job.rb
@@ -4,6 +4,8 @@ class Job < ApplicationRecord
   has_many :reviews
   has_many :users, through: :reviews
 
+  scope :alpha, -> { order(:title) }
+
   def category_attributes=(attr)
     self.category = Category.find_or_create_by(attr) if !attr[:name].blank?
   end
