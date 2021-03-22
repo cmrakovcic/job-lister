01778ec468ae737e2ad96e347fc5547eba2f04ca
Christopher Mrakovcic <cmrakovcic@aol.com>
2021-03-22 16:46:26 -0400

    cadded a jobs_helper

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,2 +1,17 @@
 module JobsHelper
+    def index_display_header
+        if @user
+            content_tag(:h1, "#{@user.username}'s Jobs:")
+        else
+            content_tag(:h2, "All Jobs")
+        end
+    end
+
+    def display_jobs
+        if @user.jobs.empty?
+            tag.h2(link_to("No jobs yet - create a job here", new_job_path))
+        else
+            content_tag(:h2, "Your Jobs:")
+        end
+    end
 end
