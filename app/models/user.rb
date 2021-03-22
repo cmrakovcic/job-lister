887d0976def3bdabe5466ad8b402e32e9f0243c1
Christopher Mrakovcic <cmrakovcic@aol.com>
2021-03-22 12:29:00 -0400

    added categories to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
     has_many :reviews
     has_many :reviewed_jobs, through: :reviews, source: :job
     has_secure_password
+    has_many :categories, through: :jobs
 end
