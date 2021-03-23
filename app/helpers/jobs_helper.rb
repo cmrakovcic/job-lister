9ee440fe49750efb53a728631f389fef36036e31
Christopher Mrakovcic <cmrakovcic@aol.com>
2021-03-23 11:15:05 -0400

    working partials implemented into views

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -11,7 +11,8 @@ module JobsHelper
         if @user.jobs.empty?
             tag.h2(link_to("No jobs yet - create a job here", new_job_path))
         else
-            content_tag(:h2, "Your Jobs:")
+            user = @user == current_user ? 'Your' : "#{@user.username}'s"
+            content_tag(:h2, "#{user} #{pluralize(@user.jobs.count, 'Job')}:")
         end
     end
 end
