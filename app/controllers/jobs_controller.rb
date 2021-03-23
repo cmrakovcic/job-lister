9a0f354464c5f2b914a36a24339c8c4d88c336e4
Christopher Mrakovcic <cmrakovcic@aol.com>
2021-03-23 17:07:50 -0400

    started implementing addition functionality, delete into app

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -49,6 +49,12 @@ class JobsController < ApplicationController
         redirect_to jobs_path if !@job
     end
 
+    def destroy
+        @job = Job.find(params[:id])
+        @job.destroy
+        redirect_to jobs_path
+    end
+
     private
 
     def job_params
